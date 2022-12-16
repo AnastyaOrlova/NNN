602b65895363508f41c79daf3d8dcf2db7c96d34
Nastyanastyy <ao.nastyanastyy@gmail.com>
2022-12-16 21:39:45 +0500

    Веб-сервис

--- erfhfjytijc.py
+++ erfhfjytijc.py
@@ -1,11 +1,46 @@
 import datetime
+import linecache
 import  os
 import datetime
+import random
+import string
 
-id = int(1)
-path = r"notes/" + str(id) + ".txt"
-time_c = datetime.datetime.fromtimestamp(os.path.getctime(path))
-time_u = os.path.getmtime(path)
+#id = int(1)
+#path = r"notes/" + str(id) + ".txt"
+#time_c = datetime.datetime.fromtimestamp(os.path.getctime(path))
+#time_u = os.path.getmtime(path)
 
-print(time_c)
-print(time_u)
+#print(time_c)
+#print(time_u)
+
+#letters = string.ascii_lowercase
+#token = ''.join(random.sample(letters, 4))
+#t = open("tokens.txt", "a")
+#id = 11
+#line = str('\n' + str(id) + ". " + token)
+#t.write(line)
+
+#token = str("rrrr")
+#t = linecache.getline('tokens.txt', 1)
+#t = t.split()
+#t = t[1]
+#if token == t:
+#    print(t)
+n = 1
+e = 1
+token = str('guiy')
+file1 = open("tokens.txt", "r")
+id = 1
+lines = file1.readlines()
+for line in lines:
+    a = line.split(". ")
+    if a[0] == str(id):
+        t = str(a[1])
+        print(t)
+
+print(type(t))
+print(type(token))
+if token == t:
+    print("уже")
+else:
+    print("хуйня")
--- web_app.py
+++ web_app.py
@@ -1,5 +1,5 @@
+import linecache
 from typing import List
-
 import fastapi
 import os
 from model import NoteInfoResponse
@@ -9,76 +9,140 @@ from model import NoteListResponse
 from model import NoteDeleteResponse
 from model import NoteUpdateResponse
 import datetime
-
+import random
+import string
 api_router = fastapi.APIRouter()
 
 @api_router.post("/create_note", response_model = NoteCreateResponse)
 def create_note(text: str, token: str):
-    f1 = os.listdir("notes")
-    m_id = []
-    for i in range(len(f1)):
-        a: list[str] = f1[i].split(".")
-        m_id.append(a[0])
-        p = int(max(m_id))
-        id = p + 1
+    t = linecache.getline('tokens.txt', 1)
+    t = t.split()
+    t = t[1]
+    if token == t:
+        f1 = os.listdir("notes")
+        m_id = []
+        for i in range(len(f1)):
+            a: list[str] = f1[i].split(".")
+            m_id.append(a[0])
+            p = int(max(m_id))
+            id = p + 1
+        my_file = open('notes/' + str(id) + ".txt", "w+")
+        my_file.write(text)
+        letters = string.ascii_lowercase
+        token = ''.join(random.sample(letters, 4))
+        t = open("tokens.txt", "a")
+        line = str('\n' + str(id) + ". " + token)
+        t.write(line)
 
-    my_file = open('notes/' + str(id) + ".txt", "w+")
-    my_file.write(text)
+        return NoteCreateResponse(
+            id = int(id),
+        )
+    else:
+        return NoteCreateResponse(
+            id = 0
+        )
 
-    return NoteCreateResponse(
-    id = int(id),
-    )
 
 @api_router.get("/read_note", response_model = NoteTextResponse)
-def read_note(id: str):
-    my_file = open('notes/' + str(id) + ".txt", "r")
-    text = my_file.read()
+def read_note(id: str, token: str):
+    file1 = open("tokens.txt", "r")
+    id = 7
+    lines = file1.readlines()
+    for line in lines:
+        a = line.split(".")
+        if a[0] == str(id):
+            t = a[1]
 
-    return NoteTextResponse(
-    id = int(id),
-    text = str(text)
-    )
+    if token == t:
+        my_file = open('notes/' + str(id) + ".txt", "r")
+        text = my_file.read()
+
+        return NoteTextResponse(
+        id = int(id),
+        text = str(text)
+        )
+
+    else:
+        return NoteTextResponse(
+            id = 0
+        )
 
 @api_router.delete("/delete_note", response_model = NoteDeleteResponse)
-def delete_note(id: int):
-    os.remove('notes/' + str(id) + ".txt")
+def delete_note(id: int, token: str):
+    n = int(id)
+    t = linecache.getline('tokens.txt', n)
+    t = t.split()
+    t = t[1]
+    if token == t:
+        os.remove('notes/' + str(id) + ".txt")
 
-    return NoteDeleteResponse(
-    id = int(id)
-    )
+        return NoteDeleteResponse(
+        id = int(id)
+        )
+    else:
+        return NoteDeleteResponse(
+        id = 0
+        )
 
 @api_router.put("/update_note", response_model = NoteUpdateResponse)
-def update_note(id: int, text: str):
-    global time_update
-    my_file = open('notes/' + str(id) + ".txt", "w+")
-    my_file.write(text)
+def update_note(id: int, text: str, token: str):
+    n = int(id)
+    t = linecache.getline('tokens.txt', n)
+    t = t.split()
+    t = t[1]
+    if token == t:
+        my_file = open('notes/' + str(id) + ".txt", "w+")
+        my_file.write(text)
 
-    return NoteUpdateResponse(
-    id = int(id),
-    )
+        return NoteUpdateResponse(
+        id = int(id)
+        )
+    else:
+        return NoteUpdateResponse(
+        id = 0
+        )
 
 @api_router.get("/list_note", response_model = NoteListResponse)
-def list_note():
-    f1 = os.listdir("notes")
-    m_id = []
-    for i in range(len(f1)):
-        a = f1[i].split(".")
-        m_id.append(int(a[0]))
+def list_note(token: str):
+    n = int(id)
+    t = linecache.getline('tokens.txt', n)
+    t = t.split()
+    t = t[1]
+    if token == t:
+        f1 = os.listdir("notes")
+        m_id = []
+        for i in range(len(f1)):
+            a = f1[i].split(".")
+            m_id.append(int(a[0]))
 
-    return NoteListResponse(
-    note_list = m_id
-    )
+        return NoteListResponse(
+        note_list = m_id
+        )
+    else:
+        return NoteListResponse(
+        note_list = " "
+        )
 
 @api_router.get("/info_note", response_model = NoteInfoResponse)
-def info_note(id: int):
-    path = r"notes/" + str(id) + ".txt"
-    time_c = datetime.datetime.fromtimestamp(os.path.getctime(path))
-    time_u = datetime.datetime.fromtimestamp(os.path.getmtime(path))
+def info_note(id: int, token: str):
+    n = int(id)
+    t = linecache.getline('tokens.txt', n)
+    t = t.split()
+    t = t[1]
+    if token == t:
+        path = r"notes/" + str(id) + ".txt"
+        time_c = datetime.datetime.fromtimestamp(os.path.getctime(path))
+        time_u = datetime.datetime.fromtimestamp(os.path.getmtime(path))
 
-    return NoteInfoResponse(
-    created_at = time_c,
-    updated_at = time_u
-    )
+        return NoteInfoResponse(
+        created_at = time_c,
+        updated_at = time_u
+        )
+    else:
+        return NoteInfoResponse(
+        created_at = 0,
+        updated_at = 0
+        )
 
 
 
